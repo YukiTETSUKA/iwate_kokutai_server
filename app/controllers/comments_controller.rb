80a202de9bd5ff65d4f0facde45154a5c16fb133
Yuki Tetsuka <tetsuka@cheekit.com>
2014-12-13 20:29:30 +0900

    seed更新，県へのコメントがされる度にポイントが増加

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,11 +2,12 @@ class CommentsController < ApplicationController
   protect_from_forgery except: :create
 
   def show
-    render :json => Comment.all if params[:prefecture_id].to_i == 47 # 全都道府県へのコメントを取得してくる
-    render :json => Comment.where(prefecture_id: params[:prefecture_id]) unless params[:prefecture_id].to_i == 47
+    render :json => Comment.all if params[:prefecture_id].to_i == 0 # 全都道府県へのコメントを取得してくる
+    render :json => Comment.where(prefecture_id: params[:prefecture_id]) unless params[:prefecture_id].to_i == 0
   end
 
   def create
     Comment.create(:comment => params[:comment], :prefecture_id => params[:prefecture_id].to_i)
+    Prefecture.calc_point params[:prefecture_id]
   end
 end
